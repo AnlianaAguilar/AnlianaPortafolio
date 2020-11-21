07e1fd4c2d942cd79428305e011fae5b7212cd0d
anlianaaguilar <anlianaaguilar@gmail.com>
2020-11-21 18:48:30 -0300

    Cambio

--- src/views/home.js
+++ src/views/home.js
@@ -14,7 +14,7 @@ export const HomePage = () => {
       
         <div className="hijo">
              <p className="hijoText">Developer Front-End e Ingeniera Industrial, me caracterizo por tener iniciativa, ser proactiva, autodidacta, organizada, con alto sentido de responsabilidad, con capacidad de trabajar en equipo.<br/>
-              En la actualidad decidí cambiar de rugro laboral y profesional, para convertirme en una desarrolladora web, realicé una bootcamp con la Fundación Laboratoria, desarrollé más de 6 proyectos reales y practicos en los que puse en practica Javascript, HTML, CCS, Node.Js, Read, GitHub. 
+              En la actualidad decidí cambiar de rugro laboral y profesional, para convertirme en una desarrolladora web, realicé una bootcamp con la Fundación Laboratoria, desarrollé más de 6 proyectos reales y practicos en los que puse en practica Javascript, HTML, CSS, Node.Js, React, GitHub. 
               <br/>
               Cuento con una amplia experiencia como ingeniera industrial lo que me permite tener un buen  desenvolvimiento en planeación, desarrollo y ejecución de proyectos. 
               <br/>Datos free: Me gusta bailar, hacer ejecicios, los perritos y fuí cheff de un restaurante de comida Chilena..</p>
